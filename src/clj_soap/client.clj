24e4c880cfa4a848bdef0e23ad41c5f36961d4bd
Martin Zürn <martin@weltundall.de>
2017-11-27 19:47:55 +0100

    Added support for arbitrary parameters
    - Feature: Use of hash map for parameters
    - Feature: Parameters not defined in the WSDL will be appended to the request
    - Fixed: Elements that didn't receive a name were not added to the request accordingly
    - Fixed: Namespace of root message wasn't determined correctly
    - Fixed: Complex structures (e.g. objects) were not always handled correctly.
      Added handling for maps and plain objects, throwing on arrays
    - Fixed: When adding arbitrary objects, `setText` was called with non-string values
    - Added tests
    - Removed broken test

--- src/clj_soap/client.clj
+++ src/clj_soap/client.clj
@@ -25,11 +25,16 @@
 
 (defn axis-op-name
   [axis-op]
-  (.getLocalPart (.getName axis-op)))
+  (some-> axis-op
+          (.getName)
+          (.getLocalPart)))
 
 (defn axis-op-namespace
   [axis-op]
-  (.getNamespaceURI (.getName axis-op)))
+  (some-> axis-op
+          (.getMessage "Out")
+          (.getElementQName)
+          (.getNamespaceURI)))
 
 (defn axis-op-args
   [axis-op]
@@ -37,7 +42,7 @@
                                     (iterator-seq (.getMessages axis-op))))
                      .getSchemaElement .getSchemaType
                      .getParticle .getItems seq)]
-    {:name (.getName elem)
+    {:name (or (.getName elem) (some-> elem .getWireName .getLocalPart))
      :type (some-> elem .getSchemaType .getName keyword)
      :elem elem}))
 
@@ -62,37 +67,64 @@
 
 (defn make-om-elem
   ([factory tag-name]
-   (.createOMElement
-     factory (javax.xml.namespace.QName. tag-name)))
+   (.createOMElement factory
+                     (if (instance? QName tag-name)
+                       tag-name
+                       (QName. tag-name))))
   ([factory tag-name value-type value]
-   (doto (.createOMElement
-           factory (javax.xml.namespace.QName. tag-name))
+   (doto (make-om-elem factory tag-name)
      (.setText (if (or (nil? value) (nil? value-type))
                  ; support optional parameters and values without defined types
-                 value
+                 (str value)
                  (obj->soap-str value value-type))))))
 
 (defn map-obj->om-element
-  [factory op argtype argval]
-  (let [outer-element (make-om-elem factory (:name argtype))]
-    (doseq [[key val] argval]
-      (.addChild outer-element
-                 (make-om-elem factory (name key) (:type argtype) val)))
-    outer-element))
+  [factory op arg-type arg-key arg-val]
+  (let [tag-name (QName. (axis-op-namespace op)
+                         (or (:name arg-type) (name arg-key)))]
+    (cond
+      (seq? arg-val) (throw (UnsupportedOperationException. "XML Arrays not yet supported"))
+      (map? arg-val) (let [outer-element (make-om-elem factory tag-name)]
+                       (doseq [[key val] arg-val]
+                         (.addChild outer-element
+                                    (make-om-elem factory
+                                                  (QName. (axis-op-namespace op) (name key))
+                                                  (:type arg-type)
+                                                  val)))
+                       outer-element)
+      :else (make-om-elem factory tag-name (:type arg-type) val))))
+
+(defn add-request-parameter!
+  "Adds a single parameter to the request.
+  Uses `map-obj->om-element` if `arg-type` is unknown (i.e. not a primitive) or `complex-args` is enabled.
+  Otherwise, it creates a new tag and uses `obj->soap-str` to determine the tag value."
+  [op request factory options arg-type arg-key arg-val]
+  (.addChild request
+             (if (or (nil? (:type arg-type)) (:complex-args options false))
+               (map-obj->om-element factory op arg-type arg-key arg-val)
+               (doto (.createOMElement
+                       factory (QName. (axis-op-namespace op) (:name arg-type)))
+                 (.setText (obj->soap-str arg-val (:type arg-type)))))))
 
 (defn make-request
-  [op options & args]
+  [op options args]
   (let [factory (OMAbstractFactory/getOMFactory)
         request (.createOMElement
                   factory (QName. (axis-op-namespace op) (axis-op-name op)))
         op-args (axis-op-args op)]
-    (doseq [[argval argtype] (map list args op-args)]
-      (.addChild request
-                 (if (or (nil? (:type argtype)) (:complex-args options false))
-                   (map-obj->om-element factory op argtype argval)
-                   (doto (.createOMElement
-                           factory (javax.xml.namespace.QName. (axis-op-namespace op) (:name argtype)))
-                     (.setText (obj->soap-str argval (:type argtype)))))))
+
+    ;; Handle all defined parameters
+    (doseq [arg-type op-args]
+      (let [arg-key (:name arg-type)
+            arg-val ((keyword arg-key) args)]
+        (add-request-parameter! op request factory options arg-type arg-key arg-val)))
+
+    ;; Handle all undefined parameters
+    (let [op-args-keys         (map #(keyword (:name %)) op-args)
+          args-without-defined (apply dissoc args op-args-keys)]
+      (doseq [[arg-key arg-val] args-without-defined]
+        (add-request-parameter! op request factory options nil arg-key arg-val)))
+
     (log/trace "Invoking SOAP Operation:" (.getName op) "Request:" request)
     request))
 
@@ -103,8 +135,8 @@
     (xml/parse-str result-xml)))
 
 (defn client-call
-  [client op options & args]
-  (let [request (apply make-request op options args)]
+  [client op options args]
+  (let [request (make-request op options args)]
     (locking client
       (if (isa? (class op) OutOnlyAxisOperation)
         (.sendRobust client (.getName op) request)
@@ -115,20 +147,20 @@
   [client options]
   (->> (for [op (axis-service-operations (.getAxisService client))]
          [(keyword (axis-op-name op))
-          (fn soap-call [& args] (apply client-call client op options args))])
+          (fn [args] (client-call client op options args))])
        (into {})))
 
 (defn make-client
   [url & [{:keys [auth throw-faults timeout chunked? wsdl-auth headers]
-           :or {throw-faults true
-                chunked? false}}]]
+           :or   {throw-faults true
+                  chunked?     false}}]]
   (let [options (doto (Options.)
                   (.setTo (EndpointReference. url))
                   (.setProperty HTTPConstants/CHUNKED (str chunked?))
                   (.setExceptionToBeThrownOnSOAPFault throw-faults))]
 
-    ; if WSDL is password-protected, must enable access for URLConnection/connect
-    ; which is used internally by Axis2
+    ;; if WSDL is password-protected, must enable access for URLConnection/connect
+    ;; which is used internally by Axis2
     (when wsdl-auth
       (let [url-authenticator (proxy [Authenticator] []
                                 (getPasswordAuthentication []
@@ -136,44 +168,44 @@
                                                            (char-array (:password wsdl-auth)))))]
         (Authenticator/setDefault url-authenticator)))
 
-    ; support authentication when making SOAP requests
+    ;; support authentication when making SOAP requests
     (when auth
       (let [req-authenticator (doto (HttpTransportPropertiesImpl$Authenticator.)
                                 (.setUsername (:username auth))
                                 (.setPassword (:password auth)))]
         (.setProperty options HTTPConstants/AUTHENTICATE req-authenticator)))
 
-    ; enable connection timeouts
+    ;; enable connection timeouts
     (when timeout
       (.setTimeOutInMilliSeconds options timeout)
       (.setProperty options HTTPConstants/SO_TIMEOUT timeout)
       (.setProperty options HTTPConstants/CONNECTION_TIMEOUT timeout))
 
-    ; enable custom headers
+    ;; enable custom headers
     (when headers
       (let [headers-list (ArrayList.)]
         (doseq [[k v] headers]
           (.add headers-list (NamedValue. (str k) (str v))))
         (.setProperty options HTTPConstants/HTTP_HEADERS headers-list)))
 
-    ; ensure all created operation clients also have the same set of options
+    ;; ensure all created operation clients also have the same set of options
     (doto (ServiceClient. nil (AxisService/createClientSideAxisService
                                 (URL. url) nil nil options))
       (.setOverrideOptions options))))
 
 (defn client-fn
   "Creates SOAP client proxy function which must be invoked with keywordised
-  version of the SOAP function and any additional arguments
-  e.g. (client :GetData \"test1\" \"test2\").
+  version of the SOAP function and any additional arguments as keywordized hash.
+  e.g. (client :GetData {:foo \"test1\" :bar \"test2\"}).
   A map of options are required for generating the function.
   Either :base-client must be supplied (created with make-client) or the :wsdl
   URL string with :options data."
   [{:keys [wsdl options base-client]}]
   (let [; either base client must be supplied or URL with optional data
         client (or base-client (make-client wsdl options))
-        px (client-proxy client options)]
-    (fn [method-name & args]
+        px     (client-proxy client options)]
+    (fn [method-name args]
       (if-let [operation (px method-name)]
-        (apply operation args)
+        (operation args)
         (throw (IllegalArgumentException. (str "Cannot find SOAP method '" method-name "'. "
                                                "Available methods are: " (keys px))))))))
--- test/clj_soap/client_test.clj
+++ test/clj_soap/client_test.clj
@@ -11,6 +11,20 @@
                                        :content (#clojure.data.xml.Element{:attrs   {},
                                                                            :content ("<NewDataSet>\n  <Table>\n    <AirportCode>MUC</AirportCode>\n    <CityOrAirportName>MUNICH</CityOrAirportName>\n    <Country>Germany</Country>\n    <CountryAbbrviation>DE</CountryAbbrviation>\n    <CountryCode>429</CountryCode>\n    <GMTOffset>-1</GMTOffset>\n    <RunwayLengthFeet>9199</RunwayLengthFeet>\n    <RunwayElevationFeet>1737</RunwayElevationFeet>\n    <LatitudeDegree>48</LatitudeDegree>\n    <LatitudeMinute>21</LatitudeMinute>\n    <LatitudeSecond>0</LatitudeSecond>\n    <LatitudeNpeerS>N</LatitudeNpeerS>\n    <LongitudeDegree>11</LongitudeDegree>\n    <LongitudeMinute>47</LongitudeMinute>\n    <LongitudeSeconds>0</LongitudeSeconds>\n    <LongitudeEperW>E</LongitudeEperW>\n  </Table>\n  <Table>\n    <AirportCode>MUC</AirportCode>\n    <CityOrAirportName>MUNICH</CityOrAirportName>\n    <Country>Germany</Country>\n    <CountryAbbrviation>DE</CountryAbbrviation>\n    <CountryCode>429</CountryCode>\n    <GMTOffset>-1</GMTOffset>\n    <RunwayLengthFeet>9199</RunwayLengthFeet>\n    <RunwayElevationFeet>1737</RunwayElevationFeet>\n    <LatitudeDegree>48</LatitudeDegree>\n    <LatitudeMinute>21</LatitudeMinute>\n    <LatitudeSecond>0</LatitudeSecond>\n    <LatitudeNpeerS>N</LatitudeNpeerS>\n    <LongitudeDegree>11</LongitudeDegree>\n    <LongitudeMinute>47</LongitudeMinute>\n    <LongitudeSeconds>0</LongitudeSeconds>\n    <LongitudeEperW>E</LongitudeEperW>\n  </Table>\n</NewDataSet>"),
                                                                            :tag     :getAirportInformationByAirportCodeResult})}
+             result))))
+  (testing "Validate GeoIP"
+    (let [client (soap-client/client-fn {:wsdl "http://www.webservicex.net/geoipservice.asmx?WSDL"})
+          ;; IP address of apple.com
+          result (client :GetGeoIP {:IPAddress "17.142.160.59"})]
+      (is (= #clojure.data.xml.Element{:attrs   {},
+                                       :content (#clojure.data.xml.Element{:attrs   {},
+                                                                           :content (#clojure.data.xml.Element{:attrs {}, :content ("1"), :tag :ReturnCode}
+                                                                                      #clojure.data.xml.Element{:attrs {}, :content ("17.142.160.59"), :tag :IP}
+                                                                                      #clojure.data.xml.Element{:attrs {}, :content ("Success"), :tag :ReturnCodeDetails}
+                                                                                      #clojure.data.xml.Element{:attrs {}, :content ("United States"), :tag :CountryName}
+                                                                                      #clojure.data.xml.Element{:attrs {}, :content ("USA"), :tag :CountryCode}),
+                                                                           :tag     :GetGeoIPResult}),
+                                       :tag     :GetGeoIPResponse}
              result)))))
 
 (deftest test-throw-if-method-illegal
