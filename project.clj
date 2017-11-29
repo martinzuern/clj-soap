70ad4fab9acdef2afc44791263aadfedf3b11902
Martin Zürn <martin@weltundall.de>
2017-11-29 18:29:43 +0100

    Added GPG key to project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "SOAP Client using Apache Axis2."
   :url "https://gitlab.com/xapix/engineering/clj-soap"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/data.xml "0.0.8"]
@@ -12,5 +12,6 @@
                  [org.apache.axis2/axis2-transport-local "1.7.6"]
                  [org.apache.axis2/axis2-jaxws "1.7.6"]]
   :source-paths ["src" "test"]
+  :signing {:gpg-key "C07962D3"}
   )
 
