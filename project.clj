d402a68853a9057c5f3586e8324fa5af982ff033
Martin Zürn <martin@weltundall.de>
2017-11-29 15:10:07 +0100

    Updated dependencies
    Closes #3

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :wsdl "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [clj-time "0.12.0"]
-                 [org.apache.axis2/axis2-adb "1.7.4"]
-                 [org.apache.axis2/axis2-transport-http "1.7.4"]
-                 [org.apache.axis2/axis2-transport-local "1.7.4"]
-                 [org.apache.axis2/axis2-jaxws "1.7.4"]]
+                 [clj-time "0.14.2"]
+                 [org.apache.axis2/axis2-adb "1.7.6"]
+                 [org.apache.axis2/axis2-transport-http "1.7.6"]
+                 [org.apache.axis2/axis2-transport-local "1.7.6"]
+                 [org.apache.axis2/axis2-jaxws "1.7.6"]]
   :source-paths ["src" "test"]
   )
 
